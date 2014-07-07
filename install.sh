888c31d27912d3a2e2a8a66e7b93e38d9463a019
Cgg <clement.geiger@gmail.com>
2014-07-07 10:30:15 +0200

    correct install script

--- install.sh
+++ install.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/zsh
 
+if [ ! -n "~/bin" ]
+then
+  mkdir ~/bin
+fi
+
+link_exist() {
+  if [ -h $1 ]
+  then
+    return 0
+  fi
+  return 1
+}
+
 if git --version
 then
   echo "git installed."
@@ -8,13 +21,20 @@ else
   sudo apt-get install -y git
 fi
 
-if zsh -v
+if zsh --version
 then
   echo "zsh installed."
 else
   echo "Installing zsh..."
   sudo apt-get install -y zsh
   chsh -s `which zsh`
+fi
+
+if which lolcat
+then
+  echo "lolcat installed"
+else
+  gem install --user-install lolcat
 fi
 
 if [ ! -n "$ZSH" ]
@@ -37,6 +57,14 @@ else
   sudo apt-get install -y cowsay
 fi
 
+if which fortune
+then
+  echo "fortune installed."
+else
+  echo "Installing fortune..."
+  sudo apt-get install -y fortune
+fi
+
 if ag --version
 then
   echo "ag installed."
@@ -45,17 +73,28 @@ else
   git clone https://github.com/ggreer/the_silver_searcher
   sudo apt-get install -y automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev
   cd the_silver_searcher
-  make -j8
-  ln -s ag ~/bin/ag
+  ./build.sh
+  AG=~/bin/ag
+  if link_exist $AG; then rm $AG; fi
+  ln -s `pwd`/ag ~/bin/ag
+  rehash
   cd ..
 fi
 
 echo "Installing fortune prompt..."
-ln -s bin/fortune-prompt.sh ~/bin/fortune-prompt.sh
+FPROMPT=~/bin/fortune-prompt.sh
+if link_exist $FPROMPT; then rm $FPROMPT; fi
+ln -s `pwd`/bin/fortune-prompt.sh $FPROMPT
+
 echo "Installing zshrc..."
-ln -s zshrc ~/.zshrc
+ZSHRC=~/.zshrc
+if link_exist $ZSHRC; then rm $ZSHRC; fi
+ln -s `pwd`/zshrc $ZSHRC
+
 echo "Installing gitconfig..."
-ln -s  gitconfig ~/.gitconfig
+GITCFG=~/.gitconfig
+if link_exist $GITCFG; then rm $GITCFG; fi
+ln -s  `pwd`/gitconfig $GITCFG
 
 echo "Done."
 
